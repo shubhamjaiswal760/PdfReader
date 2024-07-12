26ac40068ebbe6c705c4a62eb5baa1dd3b500b59
shubham jaiswal <shubhamjais760@gmail.com>
2024-07-12 15:19:03 +0530

    2nd commit

---
+++ app/src/main/java/com/example/pdfreader/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pdfreader
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var showBtn:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showBtn=findViewById(R.id.showBtn)
+        showBtn.setOnClickListener{
+            val intent=Intent(this@MainActivity,ViewPdfActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
